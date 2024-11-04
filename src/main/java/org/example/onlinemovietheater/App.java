4fe5dd0ce59de76e0f5c9ef14961802aec8af226
egor <Egor78396@gmail.com>
2024-11-04 16:37:43 +0300

    change jdk to 17

--- src/main/java/org/example/onlinemovietheater/App.java
+++ src/main/java/org/example/onlinemovietheater/App.java
@@ -4,8 +4,7 @@ package org.example.onlinemovietheater;
  * Hello world!
  *
  */
-public class App 
-{
+public class App {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
