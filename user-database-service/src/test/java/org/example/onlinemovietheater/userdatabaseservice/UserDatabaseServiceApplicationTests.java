65c6fabbbfbd262df8b92adbb8f93ea4f9476c28
egor <Egor78396@gmail.com>
2024-11-06 16:34:43 +0300

    user-database-service(configurade)

---
+++ user-database-service/src/test/java/org/example/onlinemovietheater/userdatabaseservice/UserDatabaseServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.onlinemovietheater.userdatabaseservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserDatabaseServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
