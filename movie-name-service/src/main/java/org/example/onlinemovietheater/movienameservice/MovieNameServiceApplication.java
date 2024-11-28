c5cfc3be6c78ad463d6ef8b5806701f7219fc2b0
egor <Egor78396@gmail.com>
2024-11-28 18:57:12 +0300

    movie-name-service created

---
+++ movie-name-service/src/main/java/org/example/onlinemovietheater/movienameservice/MovieNameServiceApplication.java
@@ -0,0 +1,13 @@
+package org.example.onlinemovietheater.movienameservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieNameServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovieNameServiceApplication.class, args);
+    }
+
+}
